fedcfd925ed78b24eac6d27be6c02b2f1cf73332
masaki0087 <pcmv2846@yahoo.co.jp>
2025-09-05 19:24:47 +0900

    7章まで完了

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,8 +1,19 @@
 import { getNewsList } from "@/app/_libs/microcms";
 import NewsList from "../_components/NewsList";
+import Pagination from "../_components/Pagination";
+import Searchfield from "../_components/SearchField";
+import { NEWS_LIST_LIMIT } from "../_constants";
 
 export default async function Page() {
-    const { contents: news } = await getNewsList();
+    const { contents: news, totalCount } = await getNewsList({
+        limit: NEWS_LIST_LIMIT,
+    });
 
-    return <NewsList news={news} />;
+    return (
+        <>
+            <Searchfield />
+            <NewsList news={news} />
+            <Pagination totalCount={totalCount} />
+        </>
+    )
 }
